3d01d633976da614c624a51cc4d43526f5b0c5fb
Awadje <michaelawad85@gmail.com>
2017-08-28 10:46:32 +0200

    Render single quote in quote grid. Start new Quote

--- src/components/QuoteGrid.vue
+++ src/components/QuoteGrid.vue
@@ -1,16 +1,20 @@
 <template>
-    <div class="row">
-
-    </div>
+<div class="row">
+  <app-quote v-for="quote in quotes">{{ quote }}</app-quote>
+</div>
 </template>
 
 <script>
+import Quote from './Quote.vue';
+
 export default {
-    props: ['quotes'] 
+  props: ['quotes'],
+  components: {
+    appQuote: Quote
+  }
 }
 </script>
 
 <style>
 
 </style>
-
